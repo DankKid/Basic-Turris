83672ec2c833dfb4cff3fad0c065d5749f783e03
Connor <61102529+Metater@users.noreply.github.com>
2022-06-08 18:43:00 -0400

    Stuff

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private bool generateGrid;
+    [SerializeField] private bool checkGrid;
+
+    [SerializeField] private Transform grid;
+    [SerializeField] private GameObject hex;
+
+    private readonly List<(GameObject, GameObject)> gridObjects = new();
+
+    private void OnValidate()
+    {
+        if (checkGrid)
+        {
+            foreach ((GameObject wall, GameObject path) in gridObjects)
+            {
+                if (wall != null)
+                {
+                    Destroy(path);
+                }
+                if (path != null)
+                {
+                    Destroy(wall);
+                }
+            }
+        }
+
+        if (generateGrid)
+        {
+            foreach ((GameObject wall, GameObject path) in gridObjects)
+            {
+                if (wall != null)
+                {
+                    Destroy(wall);
+                }
+                if (path != null)
+                {
+                    Destroy(path);
+                }
+            }
+            gridObjects.Clear();
+
+            // x: 15.5*1.73205080756
+            // z: -22.75: (15*1.5)?
+            // ratio: 1.18007868132
+            // -26.84679
+            // -22.75
+            // 2 2 2
+            generateGrid = false;
+            for (int z = 0; z < 16; z++)
+            {
+                for (int x = 0; x < 16; x++)
+                {
+                    float xPos = x * 1.73205080756f;
+                    float zPos = z * 1.5f;
+                    if (z % 2 == 1)
+                    {
+                        xPos += 0.86602540378f;
+                    }
+
+                    GameObject wall = Instantiate(hex, new Vector3(xPos, 0.35f, zPos), Quaternion.Euler(-90, 0, 0), grid);
+  
+                    GameObject path = Instantiate(hex, new Vector3(xPos, 0.1f, zPos), Quaternion.Euler(-90, 0, 0), grid);
+                    path.transform.localScale = new Vector3(1, 1, 0.5f);
+
+                    gridObjects.Add((wall, path));
+                }
+            }
+        }
+    }
+}
