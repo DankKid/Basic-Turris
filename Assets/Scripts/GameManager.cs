e14a26a5a8c706d467281d76de3107a375cccea5
Connor <61102529+Metater@users.noreply.github.com>
2022-06-08 19:32:11 -0400

    Better editor

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,56 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private bool generateGrid;
-    [SerializeField] private bool checkGrid;
 
     [SerializeField] private Transform grid;
     [SerializeField] private GameObject hex;
     [SerializeField] private Vector2Int size;
+    [SerializeField] private string map;
 
-    private readonly List<(GameObject, GameObject)> gridObjects = new();
+    private readonly List<GameObject> hexes = new();
 
     private void OnValidate()
     {
-        if (checkGrid)
-        {
-            checkGrid = false;
-
-            foreach ((GameObject wall, GameObject path) in gridObjects)
-            {
-                if (wall != null)
-                {
-                    Destroyer.DestroyOnUpdate.Add(path);
-                }
-                else
-                {
-                    if (path != null)
-                    {
-                        Destroyer.DestroyOnUpdate.Add(wall);
-                    }
-                }
-            }
-        }
-
         if (generateGrid)
         {
             generateGrid = false;
 
-            foreach ((GameObject wall, GameObject path) in gridObjects)
+            foreach (GameObject go in hexes)
             {
-                if (wall != null)
+                if (go != null)
                 {
-                    Destroyer.DestroyOnUpdate.Add(wall);
-                }
-                if (path != null)
-                {
-                    Destroyer.DestroyOnUpdate.Add(path);
+                    Destroyer.DestroyOnUpdate.Add(go);
                 }
             }
-            gridObjects.Clear();
+
+            TextAsset textFile = (TextAsset)Resources.Load("Maps/" + map);
+            string[] rows = textFile.text.Split(Environment.NewLine);
+            for (int y = 0; y < rows.Length; y++)
+            {
+                string row = rows[y].Replace(" ", "");
+                for (int x = 0; x < row.Length; x++)
+                {
+                    float xPos = x * 1.73205080756f;
+                    float zPos = y * 1.5f;
+                    if (y % 2 == 1)
+                    {
+                        xPos += 0.86602540378f;
+                    }
+
+                    switch (row[x])
+                    {
+                        case 'X':
+                            GameObject path = Instantiate(hex, new Vector3(xPos, 0.1f, zPos), Quaternion.Euler(-90, 0, 0), grid);
+                            path.transform.localScale = new Vector3(1, 1, 0.5f);
+                            hexes.Add(path);
+                            break;
+                        case 'O':
+                            GameObject wall = Instantiate(hex, new Vector3(xPos, 0.35f, zPos), Quaternion.Euler(-90, 0, 0), grid);
+                            hexes.Add(wall);
+                            break;
+                    }
+                }
+            }
 
             // x: 15.5*1.73205080756
             // z: -22.75: (15*1.5)?
@@ -58,25 +63,6 @@ public class GameManager : MonoBehaviour
             // -26.84679
             // -22.75
             // 2 2 2
-            for (int z = 0; z < size.y; z++)
-            {
-                for (int x = 0; x < size.x; x++)
-                {
-                    float xPos = x * 1.73205080756f;
-                    float zPos = z * 1.5f;
-                    if (z % 2 == 1)
-                    {
-                        xPos += 0.86602540378f;
-                    }
-
-                    GameObject wall = Instantiate(hex, new Vector3(xPos, 0.35f, zPos), Quaternion.Euler(-90, 0, 0), grid);
-  
-                    GameObject path = Instantiate(hex, new Vector3(xPos, 0.1f, zPos), Quaternion.Euler(-90, 0, 0), grid);
-                    path.transform.localScale = new Vector3(1, 1, 0.5f);
-
-                    gridObjects.Add((wall, path));
-                }
-            }
         }
     }
 }
