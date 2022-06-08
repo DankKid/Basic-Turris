18dd432b5f1ce2342648cf3f179a54be35e8dd72
DankKid <jmanholt929@gmail.com>
2022-06-08 17:35:04 -0400

    Stuff

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -35,5 +35,29 @@ public class TitleScreen : MonoBehaviour
         Debug.Log("Quit");
     }        
 
+    public void Map1()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Map2()
+    {
+        SceneManager.LoadScene(2);
+    }
+    public void Map3()
+    {
+        SceneManager.LoadScene(3);
+    }
+    public void Map4()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void Map5()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void Map6()
+    {
+        SceneManager.LoadScene(6);
+    }
 
 }
