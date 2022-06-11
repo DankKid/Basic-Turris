a76fd7cd8f2d41ed1671c2ac7a8fd90fdab82991
DankKid <jmanholt929@gmail.com>
2022-06-11 18:46:27 -0400

    Crazy Title Stuff

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -2,22 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class TitleScreen : MonoBehaviour
 {
     [SerializeField] Animator mapAnim;
     [SerializeField] Animator settingAnim;
+    [SerializeField] Animator gunAnim;
+    [SerializeField] Animator statAnim;
 
+    [SerializeField] GameObject selected;
+    [SerializeField] TMP_Text statText;
+
+    [SerializeField] List<GameObject> lockedMaps = new List<GameObject>();
+    [SerializeField] List<GameObject> lockedGuns = new List<GameObject>();
+
+
+
+    #region general
     public void PlayGame()
     {
         //Open map menu
-        mapAnim.SetTrigger("Open");
+        mapAnim.SetTrigger("OpenMap");
     }
 
     public void Back()
     {
-        mapAnim.SetTrigger("Close");
+        mapAnim.SetTrigger("CloseMap");
     }
+
+    public void GunSelection()
+    {
+        gunAnim.SetTrigger("OpenGun");
+    }
+
+    public void GunBack()
+    {
+        gunAnim.SetTrigger("CloseGun");
+    }
+
 
     public void Settings()
     {
@@ -33,8 +56,21 @@ public class TitleScreen : MonoBehaviour
     {
         Application.Quit();
         Debug.Log("Quit");
-    }        
+    }
+    
+    public void Stats()
+    {
+        statAnim.SetTrigger("OpenStats");
+    }
+    public void CloseStats()
+    {
+        statAnim.SetTrigger("CloseStats");
+    }
+    
+    
+    #endregion
 
+    #region maps
     public void Map1()
     {
         SceneManager.LoadScene(1);
@@ -59,5 +95,114 @@ public class TitleScreen : MonoBehaviour
     {
         SceneManager.LoadScene(6);
     }
+    #endregion
+
+
+
+
+
+
+    private void Start()
+    {
+        if(PlayerPrefs.GetString("selectedGun") == "")
+        {
+            PlayerPrefs.SetString("selectedGun", "pistol");
+
+
+            PlayerPrefs.SetInt("bulletsShot", 0);
+            PlayerPrefs.SetInt("enemiesKilled", 0);
+            PlayerPrefs.SetInt("wavesCompleted", 0);
+            PlayerPrefs.SetInt("pointsGained", 0);
+            PlayerPrefs.SetInt("pointsSpent", 0);
+
+
+
+
+        }
+        switch (PlayerPrefs.GetString("selectedGun"))
+        {
+            case "pistol":
+                selected.transform.localPosition = new Vector3(-350, 85, 0);
+                break;
+            case "rifle":
+                selected.transform.localPosition = new Vector3(0, 85, 0);
+                break;
+            case "shotgun":
+                selected.transform.localPosition = new Vector3(350, 85, 0);
+                break;
+        }
+
+        checkUnlock();
+
+        statText.text = "Bullets Shot: " + PlayerPrefs.GetInt("bulletsShot") + "\nEnemies Killed: " + PlayerPrefs.GetInt("enemiesKilled") + "\nWaves Completed: " + PlayerPrefs.GetInt("wavesCompleted") + "\nPoints Gained: " + PlayerPrefs.GetInt("pointsGained") + "\nPoints Spent: " + PlayerPrefs.GetInt("pointsSpent");
+
+
+
+    }
+
+
+
+    #region gun
+
+    public void SelectedPistol()
+    {
+        selected.transform.localPosition = new Vector3(-350, 85, 0);
+        
+        PlayerPrefs.SetString("selectedGun", "pistol");
+    }
+    public void SelectedRifle()
+    {
+        selected.transform.localPosition = new Vector3(0, 85, 0);
+        PlayerPrefs.SetString("selectedGun", "rifle");
+    }
+
+    public void SelectedShotgun()
+    {
+        selected.transform.localPosition = new Vector3(350, 85, 0);
+        PlayerPrefs.SetString("selectedGun", "shotgun");
+    }
+    #endregion
+
+
+    #region unlocks
+
+    void checkUnlock()
+    {
+        switch (PlayerPrefs.GetInt("playerLevel"))
+        {
+            case 1:
+                lockedMaps[0].SetActive(false);
+                break;
+            case 2:
+                lockedMaps[0].SetActive(false);
+                lockedMaps[1].SetActive(false);
+                lockedGuns[0].SetActive(false);
+                break;
+            case 3:
+                lockedMaps[0].SetActive(false);
+                lockedMaps[1].SetActive(false);
+                lockedMaps[2].SetActive(false);
+                break;
+            case 4:
+                lockedMaps[0].SetActive(false);
+                lockedMaps[1].SetActive(false);
+                lockedMaps[2].SetActive(false);
+                lockedMaps[3].SetActive(false);
+                lockedGuns[1].SetActive(false);
+                break;
+            case 5:
+                lockedMaps[0].SetActive(false);
+                lockedMaps[1].SetActive(false);
+                lockedMaps[2].SetActive(false);
+                lockedMaps[3].SetActive(false);
+                lockedMaps[4].SetActive(false);
+                break;
+        }
+    }
+
+
+
+
+    #endregion
 
 }
